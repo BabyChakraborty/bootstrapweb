41d9803c16de5617e9206f46d1f577a90c88b9ed
BabyChakraborty <babycu07@gmail.com>
2015-01-17 00:11:02 +0600

    practise calculator project

---
+++ cal.php
@@ -0,0 +1,66 @@
+<?php
+
+if($_POST)
+{
+    $operation = $_POST['operation'];
+    $number1 = $_POST['number1'];
+    $number2 = $_POST['number2'];
+    if($operation == "+")
+    {
+        $x = $number1 + $number2;
+    }
+    if($operation == "-")
+    {
+        $x = $number1 - $number2;
+    }
+    if($operation == "x")
+    {
+        $x = $number1 * $number2;
+    }
+    if($operation == "/")
+    {
+        $x = $number1 / $number2;
+    }
+    if($operation == "%")
+    {
+        $x = $number1 % $number2;
+    }
+    if($operation == "^")
+    {
+        $x = pow ( $number1 , $number2);
+    }
+    echo $x. " is the answer<br/>";
+}
+?>
+<html>
+<head>
+    <title>Basic Calculator</title>
+</head>
+<body>
+<form method = "post">
+    Enter first number: <input type="number" name="number1" />
+    <br/>
+    <select name = "operation">
+        <option value = "+">+</option>
+        <option value = "-">-</option>
+        <option value = "x">x</option>
+        <option value = "/">/</option>
+        <option value = "%">%(mod)</option>
+        <option value="^">^(power)</option>
+    </select>
+    <br/>
+    Enter second number: <input type="number" name="number2" />
+    <br/>
+    <input type = "submit" value ="send" />
+</form>
+<a href="graph.php">Graphing</a>
+</body>
+</html>
+
+Status
+API
+Training
+Shop
+Blog
+About
+
---
+++ cal1join.php
@@ -0,0 +1,31 @@
+
+<?php
+$action = $_REQUEST["action"];
+$numb1 = $_REQUEST["numb1"];
+$numb2 = $_REQUEST["numb2"];
+if($action == "+"){
+    $solve = "$numb1" + "$numb2";
+} elseif ($action == "-") {
+    $solve = "$numb1" - "$numb2";
+} elseif ($action == "*") {
+    $solve = "$numb1" * "$numb2";
+} elseif ($action == "/") {
+    $solve = "$numb1" / "$numb2";
+} elseif ($action == "%") {
+    $solve = "$numb1" % "$numb2";
+}
+?>
+<html>
+<body>
+<p> Solution is = <?= $solve ?>. </p><br />
+<a href="calc.php"> Another calc</a>
+</body>
+</html>
+
+Status
+API
+Training
+Shop
+Blog
+About
+
